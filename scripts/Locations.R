c2adc1efe251faa149eff854c07dd8c2b4f5496f
notPlancha <andre.plancha@hotmail.com>
2023-04-26 20:36:03 +0100

    fixed bug on locations

--- scripts/Locations.R
+++ scripts/Locations.R
@@ -18,25 +18,44 @@ path <- here("data", "locations.parquet")
 atp %>% group_by(Location) %>% 
     separate_wider_delim(Location, delim=",", names=c("City", "Country"), too_few="debug", too_many="debug") -> atpLocations
 
+atpLocations %>% select(Location, City, Country, Location_ok, Location_pieces)
+
+atpLocations %>%
+    filter(Location_ok == FALSE) %>%
+    select(Location, City, Country, Location_pieces)
+
+atpLocations %>% 
+    filter(Location_pieces > 2 & !(Location %>% endsWith("U.S.A."))) %>%
+    select(Location, City, Country, Location_pieces)
+
 read.csv("https://raw.githubusercontent.com/notPlancha/projeto-adb/main/data/countryAlias.csv") %>% distinct() -> aliases
+aliases %>% head()
 
 atp %>%
     mutate(
         Location = case_when(
             is.na(Location) | str_trim(Location) %in% c("TBA", "TBC", "TBD", "") ~ NA,
-            TRUE ~ str_extract(Location, "(?<=,\\s)[^,]+$") %>% str_squish()
+            str_detect(Location, ",") ~ str_extract(Location, "(?<=,\\s)[^,]+$") %>% str_squish(),
+            TRUE ~ Location %>% str_squish()
         ), Born = case_when(
             is.na(Born) | str_trim(Born) %in% c("TBA", "TBC", "TBD", "") ~ NA,
-            TRUE ~ str_extract(Born, "(?<=,\\s)[^,]+$") %>% str_squish()
+            str_detect(Born, ",") ~ str_extract(Born, "(?<=,\\s)[^,]+$") %>% str_squish(),
+            TRUE ~ Born %>% str_squish()
         )
     ) -> atpAliases
+atpAliases %>% select(Location, Born)
 
-aliases %>% group_by(alias) %>% summarise(country = first(country)) -> aliasesFixed
+atpAliases %>% 
+    left_join(aliases, by = join_by(Location == alias)) %>%
+    select(Location, country, code)
+
+aliases %>% group_by(alias) %>% summarise(country = first(country) %>% str_squish()) -> aliasesFixed
 
 atpAliases %>% 
     left_join(aliasesFixed, by = join_by(Location == alias)) %>% 
     mutate(tournament.location = country) %>% select(-country, -Location) %>%
     left_join(aliasesFixed, by = join_by(Born == alias)) %>% 
     mutate(principal.born = country) %>% select(-country, -Born) -> atpWCountries
+atpWCountries %>% select(tournament.location, principal.born)
 
 atpWCountries %>% arrow::write_parquet(here("data", "atpCountries.parquet"))
