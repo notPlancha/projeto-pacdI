d013c10d220bd5fe6a96b4211d375ae6e8e6bdc2
notPlancha <andre.plancha@hotmail.com>
2023-05-05 13:45:31 +0100

    fixed bug on date filters

--- scripts/toParquet.R
+++
@@ -1,9 +0,0 @@
-filePath <-here("data", "atpplayers.json")
-dest <- here("data", "atpRaw.parquet")
-
-print(paste("Getting", filePath))
-ndjson::stream_in(filePath) -> atp
-
-print(paste("Writing parquet to", dest))
-atp %>% arrow::write_parquet(dest)
-# Rscript .\scripts\toParquet.R
